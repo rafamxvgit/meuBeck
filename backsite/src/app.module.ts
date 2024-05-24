bc7eb18d1f52f488cbaf04e94b0815b259f27d15
Rafael Monteiro <rmxvmail@gmail.com>
2024-05-23 21:37:04 -0300

    feature/criação do modelo de crud do comentário

--- backsite/src/app.module.ts
+++ backsite/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProfessorModule } from './professor/professor.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [UserModule, ProfessorModule],
+  imports: [UserModule, ProfessorModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
